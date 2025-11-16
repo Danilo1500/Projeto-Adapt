026ccc22d987ab4ea274fa7e94db6217044b41a2
Daniel Couto Resende <seu-email-do-github@example.com>
2025-11-15 23:41:48 -0300

    two

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-  try {
-    mongoose.connection.on('connected', () => console.log('Database connected'))
-    await mongoose.connect(`${process.env.MONGODB_URL}/AdaptBD`)
-  } catch (error) {
-    console.log(error.message);
+  if (isConnected) {
+    // console.log("MongoDB already connected.");
+    return;
   }
-}
+
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "AdaptBD",
+    });
+
+    isConnected = conn.connections[0].readyState === 1;
+
+    console.log("MongoDB connected:", conn.connection.host);
+
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    throw new Error("Failed to connect to MongoDB");
+  }
+};
 
 export default connectDB;
