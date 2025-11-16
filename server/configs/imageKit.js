55ebede6b4eb1e34eb277690f9c56633b97901f5
Daniel Couto Resende <resendedanielcouto@gmail.com>
2025-11-16 00:20:46 -0300

    delete

--- server/configs/imageKit.js
+++
@@ -1,9 +0,0 @@
-import ImageKit from '@imagekit/nodejs';
-
-var imagekit = new ImageKit({
-  privateKey: process.env.IMAGEKIT_PRIVATE_KEY, // This is the default and can be omitted
-  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-  urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
-});
-
-export default imagekit
