8c4a380984f69d98499033d64095caf9ca5860b9
Daniel Couto Resende <resendedanielcouto@gmail.com>
2026-03-11 21:46:37 -0300

    app-fixed

--- Adapt_Frontend/src/App.jsx
+++ Adapt_Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import Home from "./pages/home/Home";
+import Home from "./pages/home/home";
 import Login from "./pages/Login/Login";
 import Feed from "./pages/Feed/Feed";
 import Messages from "./pages/Messages/Messages";
